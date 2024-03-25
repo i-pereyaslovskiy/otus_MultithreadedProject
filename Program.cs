1223e4354c1a3bc76c8b493ab41817ca1bae03e3
i-pereyaslovskiy <i.pereyaslovskiy@gmail.com>
2024-03-25 15:00:16 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+using otus_MultithreadedProject;
+
+Console.WriteLine("OS Version: " + Environment.OSVersion);
+Console.WriteLine("Processor Count: " + Environment.ProcessorCount);
+Console.WriteLine(".NET Version: " + Environment.Version);
+Console.WriteLine();
+
+int[] arrSizes = { 100_000, 1_000_000, 10_000_000 };
+
+
+
+foreach (int arrSize in arrSizes) {
+
+    int[] arr = Enumerable.Range(0, arrSize).ToArray();
+
+    Console.WriteLine("Sequential:" + SumOptions.ExecutionTimeCheck(() => SumOptions.Sum(arr)));
+    Console.WriteLine("Parallel:" + SumOptions.ExecutionTimeCheck(() => SumOptions.ThreadSum(arr)));
+    Console.WriteLine("LINQ:" + SumOptions.ExecutionTimeCheck(() => SumOptions.ParallelLINQSum(arr)));
+
+    Console.WriteLine();
+}
+
+
+
+
---
+++ SumOptions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otus_MultithreadedProject
+{
+    internal static class SumOptions
+    {
+        public static long Sum(int[]arr)
+        {
+            return arr.Sum(x => (long)x);
+        }
+
+        public static long ThreadSum(int[] arr) {
+            long sum = 0;
+
+            Thread thread = new Thread(()=> 
+            {
+                sum = arr.Sum(x => (long)x);
+            });
+            thread.Start();
+            thread.Join();
+            
+            return sum;
+        }
+
+        public static long ParallelLINQSum(int[] arr) {
+            return arr.AsParallel().Sum(x => (long)x);
+        }
+
+
+        public static string ExecutionTimeCheck(Action method) {
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            stopwatch.Start();
+            method.Invoke();
+            stopwatch.Stop();
+
+            return $"Execution time: {stopwatch.ElapsedMilliseconds} ms.";
+        }
+
+    }
+}
