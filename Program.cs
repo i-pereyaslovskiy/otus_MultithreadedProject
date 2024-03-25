5ac046387e882d0fe5d697f76bc93f34f69bdd23
i-pereyaslovskiy <i.pereyaslovskiy@gmail.com>
2024-03-25 23:19:37 +0300

    v1.2

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
 ﻿
 using otus_MultithreadedProject;
 
-int[] arrSizes = { 100_000, 1_000_000, 10_000_000/*, 100_000_000, 1_000_000_000*/ };
+int[] arrSizes = { 100_000, 1_000_000, 10_000_000, 100_000_000, 1_000_000_000 };
 
 
 
-foreach (int arrSize in arrSizes) {
+foreach (int arrSize in arrSizes)
+{
 
     int[] arr = Enumerable.Range(0, arrSize).ToArray();
 
     Console.WriteLine("Sequential:" + SumOptions.ExecutionTimeCheck(() => SumOptions.Sum(arr)));
-    Console.WriteLine("Parallel:" + SumOptions.ExecutionTimeCheck(() => SumOptions.ThreadSum(arr)));
+    Console.WriteLine("Threads:" + SumOptions.ExecutionTimeCheck(() => SumOptions.ThreadSum(arr)));
     Console.WriteLine("LINQ:" + SumOptions.ExecutionTimeCheck(() => SumOptions.ParallelLINQSum(arr)));
 
     Console.WriteLine();
--- SumOptions.cs
+++ SumOptions.cs
@@ -9,31 +9,43 @@ namespace otus_MultithreadedProject
 {
     internal static class SumOptions
     {
-        public static long Sum(int[]arr)
+        public static long Sum(int[] arr)
         {
             return arr.Sum(x => (long)x);
         }
 
-        public static long ThreadSum(int[] arr) {
+        public static long ThreadSum(int[] arr)
+        {
             long sum = 0;
+            const int THREARS_ARRAY_SIZE = 3;
+            int calculateCollectionSize = (arr.Length / THREARS_ARRAY_SIZE);
 
-            Thread thread = new Thread(()=> 
+            for (int i = 0; i < THREARS_ARRAY_SIZE; i++)
             {
-                sum = arr.Sum(x => (long)x);
-            });
-            thread.Start();
-            thread.Join();
-            
+                if (i == THREARS_ARRAY_SIZE - 1)
+                {
+                    var t = new Thread(() => Interlocked.Add(ref sum, arr.Skip(i * calculateCollectionSize).Sum(x => (long)x)));
+                    t.Start();
+                    t.Join();
+                }
+                else
+                {
+                    var t = new Thread(() => Interlocked.Add(ref sum, arr.Skip(i * calculateCollectionSize).Take(calculateCollectionSize).Sum(x => (long)x)));
+                    t.Start();
+                    t.Join();
+                }
+            }
             return sum;
         }
 
-        public static long ParallelLINQSum(int[] arr) {
+        public static long ParallelLINQSum(int[] arr)
+        {
             return arr.AsParallel().Sum(x => (long)x);
         }
 
 
-        public static string ExecutionTimeCheck(Action method) {
-
+        public static string ExecutionTimeCheck(Action method)
+        {
             Stopwatch stopwatch = Stopwatch.StartNew();
 
             stopwatch.Start();
